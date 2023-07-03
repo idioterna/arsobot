1376a41d8f2748fabc21b40bf6704216cc0a9fa3
Jure Koren <jure@hehe.si>
2023-07-03 15:45:39 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gorialis/discord.py:3.9-alpine
+
+WORKDIR /app
+
+COPY requirements.txt ./
+COPY bot.py ./
+COPY settings.py.prod ./settings.py
+RUN pip install -r requirements.txt
+
+CMD ["python", "bot.py"]
---
+++ bot.py
@@ -0,0 +1,100 @@
+import discord, time, urllib.request, random
+import settings
+from pyquery import PyQuery as pq
+from io import BytesIO
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.presences = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+cache = {}
+
+def l2u(s):
+    if type(s) == str:
+        return str(bytes(s, 'latin1'), 'utf-8')
+    if type(s) == bytes:
+        return str(s, 'utf-8')
+
+def valid_channel(name):
+    for c in settings.VALID_CHANNELS:
+        if c in name:
+            return True
+    return False
+
+def cached(what, url=None, binurl=None, duration=600):
+    global cache
+    if not cache.get(f'{what}_data') or cache.get(f'{what}_age', 0) + duration < time.time():
+        try:
+            if url:
+                newdata = pq(str(urlopen(url).read(), 'utf-8'))
+            elif binurl:
+                newdata = BytesIO(urllib.request.urlopen(binurl).read())
+            else:
+                raise ValueError("pass url or binurl")
+            cache[f'{what}_data'] = newdata
+        except:
+            pass
+        cache[f'{what}_age'] = time.time()
+    return cache[f'{what}_data']
+
+def getvreme(what='long'): # or long or full
+    napoved = cached('napoved', 'https://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl/fcast_si_text.html')
+    podatki = cached('podatki', 'https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.html')
+    text_podatki = '\n'
+    for location in settings.LOCATIONS:
+        line = podatki('td').filter(lambda i, this: location == l2u(pq(this).text()))
+        vreme_text = l2u(line.next().next()[0].text)
+        if not vreme_text: vreme_text = 'neznano'
+        stopinje_text = l2u(line.next().next().next()[0].text)
+        if not stopinje_text: stopinje_text = '??'
+        text_podatki += location.split()[-1] + ', ' + vreme_text + ', ' + stopinje_text + '°C\n'
+    text_vreme = ''
+    if what == 'long':
+        p = napoved("h2").next().next().next()
+        while True:
+            el = p[0]
+            if el.tag != 'p':
+                break
+            text_vreme += '\n' + l2u(el.text)
+            p = p.next()
+    elif what == 'full':
+        text_vreme = ''
+        p = napoved("h2:first")
+        while True:
+            el = p[0]
+            if el.tag not in ('h2', 'p'):
+                break
+            text_vreme += '\n' + l2u(el.text)
+            p = p.next()
+    elif what == 'short':
+        text_vreme = l2u(napoved("p:first")[0].text)
+    else:
+        return 'ne vem kaj je to ' + what + ', lahko je short, long, full'
+    return text_podatki + text_vreme
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('vreme') and len(message.content.split()) < 3 and valid_channel(message.channel.name):
+        try:
+            what = message.content.split()[1]
+        except IndexError:
+            what = 'long'
+        await message.channel.send('```' + getvreme(what) + '```')
+
+    if message.content.lower().startswith('radar'): and valid_channel(message.channel.name):
+        radar_gif = cached('radar', binurl='https://meteo.arso.gov.si/uploads/probase/www/observ/radar/si0-rm-anim.gif')
+        await message.channel.send(file=discord.File(radar_gif, filename='radar.gif'))
+
+client.run(settings.TOKEN)
+
