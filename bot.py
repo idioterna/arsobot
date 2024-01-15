880bcd15d70d1d3d66167908fb50698f63bea109
Jure Koren <jure@hehe.si>
2024-01-15 12:01:38 +0100

    NoneType isn't a builtin

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def l2u(s):
         return s
     if type(s) == bytes:
         return str(s, 'utf-8')
-    if type(s) == NoneType:
+    if type(s) == type(None):
         return ''
 
 def valid_channel(name):
